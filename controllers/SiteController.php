e6dcbef3e5cfdabf9645246a9bc085e31bfcbcff
Njuguna Mwangi <51181484+njugunamwangi@users.noreply.github.com>
2021-09-02 16:31:16 +0300

    Add signup function to SiteC ontroller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\SignupForm;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -84,6 +85,16 @@ class SiteController extends Controller
         return $this->render('login', [
             'model' => $model,
         ]);
+    }
+
+    public function actionSignup() {
+        $model = new SignupForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            return $this->redirect(Yii::$app->homeUrl);
+        }
+
+        return $this->render('signup', ['model' => $model]);
     }
 
     /**
